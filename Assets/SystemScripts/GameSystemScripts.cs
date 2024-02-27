4ef3de1f405d7352c84b8c6549354888245ba1df
DeveloperUchida <kouei28poiou@icloud.com>
2024-02-28 00:56:12 +0900

    DataUpdate 2024/02/28

--- Assets/SystemScripts/GameSystemScripts.cs
+++ Assets/SystemScripts/GameSystemScripts.cs
@@ -39,5 +39,6 @@ public class GameSystemScripts : MonoBehaviour
 
             Debug.LogWarning("TagDataGet!!");
         }
+        
     }
 }
--- Assets/SystemScripts/MovementRestrictionsControalScripts.cs
+++ Assets/SystemScripts/MovementRestrictionsControalScripts.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementRestrictionsControalScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float Min_x;
+    public float Min_y;
+    public float Max_x;
+    public float Max_y;
+    //移動距離の制限
     // Update is called once per frame
     void Update()
     {
-        
+        var pos = transform.position;
+
+        //Xの移動制限
+        pos.x = Mathf.Clamp(pos.x, Min_x, Max_x);
+        pos.y = Mathf.Clamp(pos.y, Min_y, Max_y);
+        transform.position = pos;
     }
 }
