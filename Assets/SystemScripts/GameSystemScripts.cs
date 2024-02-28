121fb33eff0c064689b0837a85097f81594effbc
DeveloperUchida <kouei28poiou@icloud.com>
2024-02-28 10:30:17 +0900

    DataUpdate 2024/02/28

--- Assets/SystemScripts/GameSystemScripts.cs
+++ Assets/SystemScripts/GameSystemScripts.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GameSystemScripts : MonoBehaviour
 {
     private float PlayerSpeed = 1;
+    private string answerTag = "wall";
     /*
     void Start()
     {
@@ -13,30 +14,30 @@ public class GameSystemScripts : MonoBehaviour
     //PlayerTriangleを動かすための処理コード
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, PlayerSpeed, 0);
+            transform.Translate(0, PlayerSpeed*Time.deltaTime, 0);
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-PlayerSpeed, 0, 0);
+            transform.Translate(-PlayerSpeed* Time.deltaTime, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -PlayerSpeed, 0);
+            transform.Translate(0, -PlayerSpeed*Time.deltaTime, 0);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(PlayerSpeed, 0, 0);
+            transform.Translate(PlayerSpeed*Time.deltaTime, 0, 0);
         }
     }
 
     //プレイヤーがタグに当たった場合の処理コード
     private void OnTrigger2D(Collider2D Collision)
     {
-        if (Collision.transform.CompareTag(""))   //タグのデータを取得して下記の処理を実行 
+        if (gameObject.CompareTag(answerTag))   //タグのデータを取得して下記の処理を実行 
         {   //決まり次第内容記入 2024/02/28　0：08追記
-
+            
             Debug.LogWarning("TagDataGet!!");
         }
         
